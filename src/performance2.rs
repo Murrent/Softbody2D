8d09f74914d45f9a745344ba491d2aa53bdba0ea
Tempron <wickgrenm@gmail.com>
2023-05-16 10:40:18 +0200

    Added circle test

--- src/performance2.rs
+++ src/performance2.rs
@@ -4,33 +4,41 @@ use macroquad::prelude::*;
 use nalgebra::Vector2;
 use std::time::Instant;
 
+use std::fs::File;
+use std::io::{Read, Write};
+use std::path::Path;
+
 #[macroquad::main("BasicShapes")]
 async fn main() {
     let screen_size = Vector2::new(1920.0, 1080.0);
     request_new_screen_size(screen_size.x, screen_size.y);
+    let scale = 30.0;
 
     let mut solver = Solver::new();
     solver.bounds = Bounds {
         pos: Vector2::new(0.0, 0.0),
-        size: screen_size,
+        size: screen_size / scale,
     };
+    solver.gravity = Vector2::new(0.0, 0.98);
 
-    let size = 15.0;
+    let size = 16.0;
     let pos = Vector2::new(100.0, 100.0);
-    for x in 0..10 {
-        for y in 0..10 {
+    for x in 0..20 {
+        for y in 0..20 {
             let index = Vector2::new(x as f32, y as f32);
-            solver.add_polygon(Polygon::circle(
-                size,
-                pos + Vector2::new(index.x * size * 2.0, index.y * size * 2.0),
-                10,
+            solver.add_polygon(Polygon::pressure_circle(
+                size / scale,
+                (pos + Vector2::new(index.x * 8.0 + index.x * size * 2.0, index.y * 8.0 + index.y * size * 2.0))
+                    / scale,
+                20,
                 false,
                 500.0,
+                30.0,
             ));
         }
     }
 
-    let sim_steps = 3000;
+    let sim_steps = 2000;
     let mut latencies = Vec::<f32>::new();
     latencies.resize(sim_steps, 0.0);
     let clock = Instant::now();
@@ -43,26 +51,27 @@ async fn main() {
         if frame_count >= sim_steps {
             break;
         }
+        if !is_key_pressed(KeyCode::Space) && !is_key_down(KeyCode::W) {
+            clear_background(BLACK);
 
-        clear_background(BLACK);
+            let last_frame_time = clock.elapsed().as_secs_f32();
 
-        let last_frame_time = clock.elapsed().as_secs_f32();
+            solver.update(1.0 / 60.0);
 
-        solver.update(0.005);
-
-        latencies[frame_count] = clock.elapsed().as_secs_f32() - last_frame_time;
-        frame_count += 1;
+            latencies[frame_count] = clock.elapsed().as_secs_f32() - last_frame_time;
+            frame_count += 1;
+        }
 
         for polygon in solver.get_polygons().iter() {
             let particle_count = polygon.particles.len();
             for (i, point) in polygon.particles.iter().enumerate() {
                 draw_triangle(
-                    Vec2::new(point.pos.x, point.pos.y),
+                    Vec2::new(point.pos.x, point.pos.y) * scale,
                     Vec2::new(
                         polygon.particles[(i + 1) % particle_count].pos.x,
                         polygon.particles[(i + 1) % particle_count].pos.y,
-                    ),
-                    Vec2::new(polygon.center.x, polygon.center.y),
+                    ) * scale,
+                    Vec2::new(polygon.center.x, polygon.center.y) * scale,
                     WHITE,
                 );
             }
@@ -73,4 +82,19 @@ async fn main() {
     for latency in latencies.iter() {
         println!("{}", latency);
     }
+
+    // Write out the positions to a CSV file
+    let mut file = File::create("performance2.csv");
+    if let Err(e) = file {
+        println!("Error creating file: {}", e);
+        return;
+    }
+    let mut file = file.unwrap();
+
+    let mut data = String::new();
+    for latency in latencies.iter() {
+        data += &format!("{}\n", latency);
+    }
+    file.write_all(data.as_bytes())
+        .expect("Failed to write to file");
 }
